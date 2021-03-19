59d68c0941303b12b188b901f22850a00f793b81
gabriel2302 <gabriel23002@gmail.com>
2021-03-19 07:17:00 -0300

    test: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -163,7 +163,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError('any_stack'))
   })
 
   test('Should return 500 if AddAccount throws', async () => {
@@ -182,7 +182,7 @@ describe('SignUp Controller', () => {
     }
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
-    expect(httpResponse.body).toEqual(new ServerError())
+    expect(httpResponse.body).toEqual(new ServerError('any_stack'))
   })
 
   test('Should call AddAccount with correct values', async () => {
