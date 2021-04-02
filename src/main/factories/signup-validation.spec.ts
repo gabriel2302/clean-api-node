5c9cc6f37f2ec013847ac87e232bb24fb0cfb080
gabriel2302 <gabriel23002@gmail.com>
2021-04-02 00:47:51 -0300

    refactor: move required password comparation to the ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -1,7 +1,8 @@
-import { RequiredFieldValidation } from '../../presentation/helpers/validators/required-field-validation'
 import { Validation } from '../../presentation/helpers/validators/validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validation-composite'
 import { makeSignUpValidation } from './signup-validation'
+import { RequiredFieldValidation } from '../../presentation/helpers/validators/compare-fields-validation'
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/required-field-validation'
 
 // aula 3 aplicando composite
 jest.mock('../../presentation/helpers/validators/validation-composite')
@@ -13,6 +14,7 @@ describe('SignUpValidation Factory', () => {
     for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
       validations.push(new RequiredFieldValidation(field))
     }
+    validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
--- src/presentation/helpers/validators/required-field-validation.ts
+++ src/presentation/helpers/validators/required-field-validation.ts
@@ -1,15 +1,17 @@
-import { MissingParamError } from '../../errors'
+import { InvalidParamError } from '../../errors'
 import { Validation } from './validation'
 
-export class RequiredFieldValidation implements Validation {
+export class CompareFieldsValidation implements Validation {
   private readonly fieldName: string
-  constructor (fieldName: string) {
+  private readonly fieldNameToCompare: string
+  constructor (fieldName: string, fieldNameToCompare: string) {
     this.fieldName = fieldName
+    this.fieldNameToCompare = fieldNameToCompare
   }
 
   validate (input: any): Error | null {
-    if (!input[this.fieldName]) {
-      return new MissingParamError(this.fieldName)
+    if (input[this.fieldName] !== input[this.fieldNameToCompare]) {
+      return new InvalidParamError(this.fieldNameToCompare)
     }
     return null
   }
