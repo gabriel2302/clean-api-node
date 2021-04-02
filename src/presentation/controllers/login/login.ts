1aa09be2634db4bfb8121385343169d2ea138292
gabriel2302 <gabriel23002@gmail.com>
2021-04-01 23:44:01 -0300

    feat: ensure LoginController returns 200 if valid credentials are provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,6 +1,6 @@
 import { Controller, HttpRequest, HttpResponse, EmailValidator, Authentication } from './login-protocols'
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, serverError, unauthorized } from '../../helpers/htpp-helper'
+import { badRequest, ok, serverError, unauthorized } from '../../helpers/htpp-helper'
 
 export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -27,7 +27,7 @@ export class LoginController implements Controller {
       if (!accessToken) {
         return unauthorized()
       }
-      return { statusCode: 200, body: { ok: true } }
+      return ok({ accessToken })
     } catch (error) {
       return serverError(error)
     }
