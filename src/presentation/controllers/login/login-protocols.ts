a3f39fc07a6ca5afc74fba83928b38f304149f55
gabriel2302 <gabriel23002@gmail.com>
2021-04-03 22:22:59 -0300

    refactor: move Validation to protocols

--- src/presentation/controllers/login/login-protocols.ts
+++ src/presentation/controllers/login/login-protocols.ts
@@ -1,3 +1,3 @@
 export * from '../../protocols'
 export * from '../../../domain/usecases/authentication'
-export * from '../../../presentation/helpers/validators/validation'
+export * from '../../protocols/validation'
