9d7c41b5e6079654522429542dc95eafa20e2aee
gabriel2302 <gabriel23002@gmail.com>
2021-04-15 17:47:49 -0300

    test: ensure LoadSurveysController returns 204 is LoadSurveysUseCase returns if there is no content

--- src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
+++ src/presentation/controllers/survey/load-surveys/load-surveys-controller.spec.ts
@@ -1,7 +1,7 @@
 import { SurveyModel, LoadSurveys } from './load-surveys-controller.protocols'
 import { LoadSurveysController } from './load-surveys-controller'
 import MockDate from 'mockdate'
-import { ok, serverError } from '../../../helpers/http/htpp-helper'
+import { noContent, ok, serverError } from '../../../helpers/http/htpp-helper'
 
 const makeFakeSurveys = (): SurveyModel[] => {
   return [
@@ -59,6 +59,12 @@ describe('LoadSurveys Controller', () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({})
     expect(httpResponse).toEqual(ok(makeFakeSurveys()))
+  })
+  test('Should return 204 if LoadSurveys returns empty ', async () => {
+    const { sut, loadSurveysStub } = makeSut()
+    jest.spyOn(loadSurveysStub, 'load').mockReturnValueOnce(new Promise(resolve => resolve([])))
+    const httpResponse = await sut.handle({})
+    expect(httpResponse).toEqual(noContent())
   })
   test('Should return 500 if LoadSurveys throws', async () => {
     const { sut, loadSurveysStub } = makeSut()
