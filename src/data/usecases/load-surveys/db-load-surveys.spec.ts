8d53599c39d7659d4ebd61f78ea0d23a91aa2df1
gabriel2302 <gabriel23002@gmail.com>
2021-04-23 22:29:05 -0300

    fix: make controllers returns the error message instead of the error name

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -1,6 +1,7 @@
 import { DbLoadSurveys } from './db-load-surveys'
 import { LoadSurveysRepository } from '@/data/protocols/db/survey/load-surveys-repository'
 import { SurveyModel } from '@/domain/models/survey'
+import MockDate from 'mockdate'
 
 const makeFakeSurveys = (): SurveyModel[] => {
   return [
@@ -26,6 +27,12 @@ const makeFakeSurveys = (): SurveyModel[] => {
 }
 
 const makeLoadSurveysRepository = (): LoadSurveysRepository => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+  afterAll(() => {
+    MockDate.reset()
+  })
   class LoadSurveysRepositoryStub implements LoadSurveysRepository {
     async loadAll (): Promise<SurveyModel[]> {
       return await new Promise(resolve => resolve(makeFakeSurveys()))
